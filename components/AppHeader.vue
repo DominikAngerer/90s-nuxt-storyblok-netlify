67c573a1c6f2129fe6e0bdb08e38ffd4174b6069
Dominik Angerer <dominikangerer1@gmail.com>
2018-03-18 20:40:23 +0100

    init

---
+++ components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/history">History</a></li>
+        <li><a href="/promotional">Promotional</a></li>
+        <li><a href="/informational">Informational</a></li>
+        <li><a href="/humer">Humer</a></li>
+        <li><a href="/misc">Misc</a></li>
+        <li><a href="/other">Other</a></li>
+        <!-- https://api.storyblok.com/v1/cdn/stories/${path}?version=draft&token=ZQGso3v5SVg23HOVwQ4xVwtt -->
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style>
+  .header {
+    text-align: center;
+  }
+  .header li {
+    display: inline-block;
+    padding: 10px 5px;
+    margin: 5px;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">
+    <app-header/>
+    
+
+
+  </div>
+</template>
+
+<script>
+import AppHeader from '~/components/AppHeader.vue'
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
